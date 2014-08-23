36d6a5ffab1ff2bc57c9c2294b3793aa0b67001d
Danpelgar <danpelgar@gmail.com>
2014-08-23 14:52:33 +0200

    character and assets

--- core/src/com/blogspot/ludumdaresforfun/Assets.java
+++ core/src/com/blogspot/ludumdaresforfun/Assets.java
@@ -8,10 +8,12 @@ import com.badlogic.gdx.utils.Array;
 
 public class Assets {
 	static AssetManager assetManager;
-	static Animation stand, walk, jump, standingShot, shotAnim;
+	static Animation stand, walk, jump, standingShot;
+	static Animation shotAnim;
+	static Animation bossStanding, bossGetHit;
 
 	static void loadAnimation() {
-        final String TEXTURE_ATLAS_OBJECTS = "rayaman.pack";
+        final String TEXTURE_ATLAS_OBJECTS = "characters.pack";
 		assetManager = new AssetManager();
 		assetManager.load(TEXTURE_ATLAS_OBJECTS, TextureAtlas.class);
 		assetManager.finishLoading();
@@ -19,20 +21,26 @@ public class Assets {
         TextureAtlas atlas = assetManager.get(TEXTURE_ATLAS_OBJECTS);
 		Array<AtlasRegion> regions;
 
-		regions = atlas.findRegions("rayaman_walking");
+		regions = atlas.findRegions("char_walking");
 		walk = new Animation(0.15f, regions);
 		walk.setPlayMode(Animation.PlayMode.LOOP);
 
-		regions = atlas.findRegions("rayaman_standing");
+		regions = atlas.findRegions("char_standing");
 		stand = new Animation(0, regions);
 
-		regions = atlas.findRegions("rayaman_jumping");
+		regions = atlas.findRegions("char_standing");  //change to jumping
 		jump = new Animation(0, regions);
 
-		regions = atlas.findRegions("rayaman_standing_shot");
+		regions = atlas.findRegions("char_standing");	//change to shooting
 		standingShot = new Animation(0.15f, regions);
 
-		regions = atlas.findRegions("rayaman_shot");
+		regions = atlas.findRegions("char_standing");
 		shotAnim = new Animation(0.15f, regions);
+
+		regions = atlas.findRegions("boss_standing");
+		bossStanding = new Animation(0.15f, regions);
+
+		regions = atlas.findRegions("boss_gethit");
+		bossGetHit = new Animation(0.15f, regions);
 	}
 }
