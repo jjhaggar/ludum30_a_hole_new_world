bd82e1bb4090511c6b0cc5796fd3f76e6d3af62c
Danpelgar <danpelgar@gmail.com>
2014-08-23 05:02:27 +0200

    added

--- android/src/com/blogspot/ludumdaresforfun/android/AndroidLauncher.java
+++ android/src/com/blogspot/ludumdaresforfun/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.ludum.LudumGame;
+import com.blogspot.ludumdaresforfun.LD;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new LudumGame(), config);
+		initialize(new LD(), config);
 	}
 }
--- html/src/com/blogspot/ludumdaresforfun/client/HtmlLauncher.java
+++ html/src/com/blogspot/ludumdaresforfun/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package com.blogspot.ludumdaresforfun.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.mygdx.ludum.LudumGame;
+import com.blogspot.ludumdaresforfun.LD;;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -14,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener getApplicationListener () {
-                return new LudumGame();
+                return new LD();
         }
 }
